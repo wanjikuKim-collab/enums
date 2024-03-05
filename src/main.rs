e9ec3623d77c1a6159a1ccfb9cd1d231823fc76b
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-03-05 09:30:31 +0300

    Creating instances of the enum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 enum IpAddrKind {
     V4(String),
-    v6(u8, u8, u8, u8),
+    v6([u8; 16]),
 }
 
 fn main() {
- 
+    let four = IpAddrKind::V4(String::from("127.0.0.1"));
+    let six = IpAddrKind::v6([
+    0x20, 0x01, 0x00, 0x00,
+    0x32, 0x3B, 0xDf, 0xE1,
+    0x00, 0x63, 0x00, 0x00,
+    0x00, 0xFE, 0xFB, 0xFB
+    ]);
 }
